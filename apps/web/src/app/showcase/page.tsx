454ee7e4cf633876791ea94602bcf6f02727695d
Ayush tomar <ayushtomar388@gmail.com>
2025-01-14 23:32:14 +0530

    showcase page created

---
+++ apps/web/src/app/showcase/page.tsx
@@ -0,0 +1,29 @@
+const ShowCasePage = () => {
+    return ( 
+        <div className="flex flex-col w-[94vw] max-w-7xl mx-auto min-h-svh gap-8 bg-[#0C0C0C] rounded-3xl border border-[#171717] ">
+               
+        <div className="pt-24 px-12 space-y-4">
+            <h1 className="text-4xl font-bold">Building something great with Doplo?</h1>
+            <p className="text-[#C0C0C0] text-balance">I’d love to feature it! Send me a DM on <a href="/">X</a> or email me at <a href="mailto:hi@ayushtomar.in">hi@ayushtomar.in</a></p>
+            <div className="grid grid-cols-4 gap-4 pt-10">
+                <div className="border border-[#202020] aspect-square rounded-md bg-gradient-to-b from-zinc-900 to-transparent">
+
+                </div>
+                <div className="border border-[#202020] aspect-square rounded-md bg-gradient-to-b from-zinc-900 to-transparent">
+
+                </div>
+                <div className="border border-[#202020] aspect-square rounded-md bg-gradient-to-b from-zinc-900 to-transparent">
+
+                </div>
+                <div className="border border-[#202020] aspect-square rounded-md bg-gradient-to-b from-zinc-900 to-transparent">
+
+                </div>
+
+            </div>
+        </div>
+        
+    </div> 
+     );
+}
+ 
+export default ShowCasePage;
