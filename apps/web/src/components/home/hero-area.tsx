16dd3af4a447a5792cf9783f6bf4b11e988cb1b7
Ayush tomar <ayushtomar388@gmail.com>
2025-01-14 23:19:42 +0530

    Hero area restyled

--- apps/web/src/components/home/hero-area.tsx
+++ apps/web/src/components/home/hero-area.tsx
@@ -1,33 +1,17 @@
-import Image from "next/image";
 import CopyToClipboard from "../copy-to-clipboard";
-``
+
 const HeroArea = () => {
 
 
     return ( 
         <div>
-            <div className="absolute top-0 z-[-2] min-h-svh w-screen bg-[#000000] bg-[radial-gradient(#ffffff20_1px,#000_1px)] bg-[size:20px_20px]">   
-                <div className="flex flex-col w-[94vw] mx-auto justify-center items-center h-svh gap-8">
-                    <div className="w-24 h-24 md:w-40 md:h-40 relative">
-                        <Image
-                            src="/doplosymbollogo.svg"
-                            alt="Doplo Symbol log"
-                            fill
-                        />
-                    </div>
-                    <div className="flex flex-col gap-6">
-                        <div className="w-40 h-10 relative mx-auto">
-                            <Image
-                                src="/doplotextlogo.svg"
-                                alt="Doplo Text log"
-                                fill
-                            />
-                        </div>
-                        <p className="font-semibold text-2xl text-balance text-center">
-                            A Modern UI Library for React.
-                        </p>
-                        <div className="flex gap-4 items-center justify-center">
-                            <div className="flex items-center gap-4 bg-zinc-900 w-fit py-1.5 px-3 text-zinc-400 rounded-md">
+                <div className="flex flex-col w-[94vw] max-w-6xl mx-auto min-h-screen gap-8 bg-[#0C0C0C] rounded-3xl border border-[#171717] ">
+               
+                    <div className="pt-24 pl-12 space-y-4">
+                        <h1 className="text-5xl font-bold">The Simplest UI Library</h1>
+                        <p className="text-[#C0C0C0] text-balance">You can use all the components with one command or you can just copy and paste components into your app. This hybrid approach allows you to build faster and on your own terms.</p>
+                        <div className="flex gap-4">
+                            <div className="flex items-center gap-4 bg-zinc-900 w-fit py-1.5 px-3 text-zinc-100 rounded-md">
                                 <p>npm install doplo</p>
                                 <CopyToClipboard textToCopy="npm install doplo"/>
                             </div>
@@ -36,12 +20,9 @@ const HeroArea = () => {
                             </a>
                         </div> 
                     </div>
-
                 </div>    
             </div>      
-            
-        </div>
-     );
+    );
 }
  
 export default HeroArea;
