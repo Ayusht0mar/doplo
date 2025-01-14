77e692d2d46d9d92c68a99789d50aeb7429c1cc5
Ayush tomar <ayushtomar388@gmail.com>
2025-01-14 23:19:15 +0530

    New menu items added and links updated

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,15 +1,25 @@
 import { Github} from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
     return ( 
-        <div className="flex items-center justify-between w-[94vw] mx-auto max-w-6xl my-4">
-                <div className="w-8 h-8 relative">
-                        <Image
-                            src="/doplosymbollogo.svg"
-                            alt="Doplo Symbol log"
-                            fill
-                        />
+        <div className="flex sticky top-4 items-center justify-between w-[94vw] mx-auto max-w-7xl my-4 border border-[#171717] py-3 px-4 rounded-md bg-[#0A0A0A]/50 text-[#D4D4D4] backdrop-blur-md">
+                <a href="/" className="flex gap-3 items-center">
+                        <div className="w-8 h-8 relative">
+                                <Image
+                                    src="/doplosymbollogo.svg"
+                                    alt="Doplo Symbol log"
+                                    fill
+                                />
+                        </div>
+                        <h1 className="font-semibold">Doplo</h1>
+                </a>
+                <div className="flex space-x-4">
+                    <Link href="/docs">Documentation</Link>
+                    <Link href="/showcase">Showcase</Link>
+                    <Link href="/roadmap">Roadmap</Link>
+                    <Link href="/changelog">Changelog</Link>                    
                 </div>
                 <a target="_blank" rel="noreferrer" href="https://github.com/Ayusht0mar/doplo">
                     <Github/>
