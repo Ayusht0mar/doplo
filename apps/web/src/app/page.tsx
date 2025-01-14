a8538eb87b0ad6421ec489689df8d7589bccac16
Ayush tomar <ayushtomar388@gmail.com>
2025-01-14 23:18:39 +0530

    Navbar removed

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,8 @@
 import HeroArea from "../components/home/hero-area";
-import Navbar from "../components/navbar";
 
 export default function Page(): JSX.Element {
   return (
     <div>
-      <Navbar/>  
       <HeroArea/>
     </div>
   );
