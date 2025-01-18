7d30ffc5b8a44b19fdf5c27d3d8fc1f8a30550d6
Ayush tomar <ayushtomar388@gmail.com>
2025-01-18 23:45:21 +0530

    Link added and eslint error fixed

--- apps/web/src/app/showcase/page.tsx
+++ apps/web/src/app/showcase/page.tsx
@@ -4,7 +4,7 @@ const ShowCasePage = () => {
                
         <div className="pt-12 lg:pt-24 px-4 lg:px-12 space-y-4">
             <h1 className="text-4xl font-bold">Building something great with Doplo?</h1>
-            <p className="text-[#C0C0C0] text-balance">I’d love to feature it! Send me a DM on <a href="/">X</a> or email me at <a href="mailto:hi@ayushtomar.in">hi@ayushtomar.in</a></p>
+            <p className="text-[#C0C0C0] text-balance">I&apos;d love to feature it! Send me a DM on <a href="https://x.com/justayushtomar">X</a> or email me at <a href="mailto:hi@ayushtomar.in">hi@ayushtomar.in</a></p>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-10">
                 <div className="border border-[#202020] aspect-square rounded-md bg-gradient-to-b from-zinc-900 to-transparent">
 
