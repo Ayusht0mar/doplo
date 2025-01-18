0989006bfaaa53a9ccc077b364db186cdea1bd17
Ayush tomar <ayushtomar388@gmail.com>
2025-01-18 23:29:31 +0530

    Added Build to text at the bottom.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Page(): JSX.Element {
   return (
     <div>
       <HeroArea/>
+      <p className="text-center w-full my-6">
+                    Build by <a href="https://www.ayushtomar.in/">Ayush Tomar</a>
+      </p>
     </div>
   );
 }
