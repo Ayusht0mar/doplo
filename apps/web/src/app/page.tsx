c31b3b5eba8993174f42965f29feb5efbf04a0d7
Ayush tomar <ayushtomar388@gmail.com>
2025-01-21 22:11:55 +0530

    Build by and Source code link removed

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,9 +4,6 @@ export default function Page(): JSX.Element {
   return (
     <div>
       <HeroArea/>
-      <p className="text-center w-full my-6">
-                    Build by <a href="https://www.ayushtomar.in/">Ayush Tomar</a>
-      </p>
     </div>
   );
 }
