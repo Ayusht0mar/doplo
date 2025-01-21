352d02578b01d35c34ed1e49cdc19fb83fd18bc7
Ayush tomar <ayushtomar388@gmail.com>
2025-01-21 22:14:03 +0530

    Email collector component created

--- apps/web/src/app/changelog/page.tsx
+++ apps/web/src/app/changelog/page.tsx
@@ -1,3 +1,4 @@
+import EmailCollector from "../../components/changelog/emailcollector";
 
 const ChangelogPage = () => {
     return ( 
@@ -6,15 +7,11 @@ const ChangelogPage = () => {
         <div className="pt-12 lg:pt-24 px-4 lg:px-12 space-y-4">
             <h1 className="text-4xl font-bold">Changelog</h1>
             <p className="text-[#C0C0C0] text-balance">Get our latest updates and improvements by joining our newsletter.</p>
-            {/* To do: Make the collect email feature fuctionable */}
-            <div className="flex flex-col md:flex-row  md:items-center gap-2">
-                <p className="gap-4 bg-zinc-900 w-fit py-1.5 px-3 text-zinc-100 rounded-md  min-w-full md:min-w-60">Enter your email</p>
-                <button className="text-zinc-900 bg-zinc-100 py-1.5 px-3 rounded-md font-semibold ">Subscribe</button>
-            </div>
+            <EmailCollector/>
             <div className="py-10 flex flex-col lg:flex-row gap-12 items-start">
                 <div className="flex gap-6 items-center">
                     <h3 className="text-zinc-900 bg-zinc-100 py-1.5 px-3 rounded-md font-semibold text-sm h-fit">Version 1.0.0</h3>
-                    <h4 className="text-[#C0C0C0] text-sm">04 Feb 2025</h4>
+                    <h4 className="text-[#C0C0C0] text-sm">22 January 2025</h4>
                 </div>
                 <div>
                     <h3 className="text-2xl">Initial Release</h3>
