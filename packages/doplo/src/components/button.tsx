ef684151170df0974232a3d17af2b1890b69298e
Ayush tomar <ayushtomar388@gmail.com>
2025-01-25 23:58:59 +0530

    Fix: error in import

--- packages/doplo/src/components/button.tsx
+++ packages/doplo/src/components/button.tsx
@@ -1,12 +1,9 @@
 import * as React from "react"
 import { cn } from "../lib/utils";
 
-const Button = React.forwardRef<HTMLButtonElement, React.ComponentProps<"button">>(({className , type , children, ...props} , ref ) => {
+export const Button = React.forwardRef<HTMLButtonElement, React.ComponentProps<"button">>(({className , type , children, ...props} , ref ) => {
     return ( 
        <button className={cn("bg-neutral-100 text-neutral-900 py-1.5 px-3 rounded font-semibold w-fit", className)} ref={ref} type={type} {...props} >{children}</button>
      );
 });
 Button.displayName = "Button"; // for debugging
-
- 
-export default Button;
--- packages/doplo/src/components/textarea.tsx
+++ packages/doplo/src/components/textarea.tsx
@@ -1,12 +1,9 @@
 import * as React from "react"
 import { cn } from "../lib/utils";
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<"textarea">>(({className , ...props}, ref ) => {
+export const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<"textarea">>(({className , ...props}, ref ) => {
     return ( 
        <textarea className={cn(" rounded py-1.5 px-3 focus:outline-none focus:ring-0 text-black", className)} ref={ref} {...props} />
      );
 });
 Textarea.displayName = "Textarea"; // for debugging
-
- 
-export default Textarea;
