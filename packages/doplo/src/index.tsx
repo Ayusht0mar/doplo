2ab087381869158b1cca2837479510a24ea7ed3c
Ayush tomar <ayushtomar388@gmail.com>
2025-01-25 23:57:14 +0530

    allows import via "doplo"

---
+++ packages/doplo/src/index.tsx
@@ -0,0 +1,4 @@
+export { Button } from "./components/button"
+export { Input } from "./components/input"
+export {Card, CardBody, CardHeader, CardTitle, CardDescription, CardFooter} from "./components/card"
+export { Textarea } from "./components/textarea"
