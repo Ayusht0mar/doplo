87d99ed3358da3645acdf904b91ad4060a551aef
Ayush tomar <ayushtomar388@gmail.com>
2025-01-25 23:59:29 +0530

    Docs pages layout created

---
+++ apps/web/src/app/docs/layout.tsx
@@ -0,0 +1,16 @@
+import DocsSidebar from "../../components/docs/docs-sidebar";
+
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }): JSX.Element {
+    return (
+        <div className="w-[94vw] mx-auto flex flex-row overflow-hidden bg-[#0C0C0C] border border-[#171717] rounded-xl max-w-7xl min-h-screen">
+            <DocsSidebar/>
+            <div className="p-4 w-full">
+                {children}
+            </div>
+          </div>
+    );
+  }
