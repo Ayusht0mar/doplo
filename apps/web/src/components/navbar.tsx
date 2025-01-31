fd6eee8d25daa9e765a7eb87a1d54f1a3d56c59e
Ayush tomar <ayushtomar388@gmail.com>
2025-01-31 23:40:13 +0530

    Styled

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,20 +1,22 @@
-import { Github} from "lucide-react";
+import { Github, X} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
     return ( 
         <div className="flex sticky top-4 z-10 items-center justify-between w-[94vw] mx-auto max-w-7xl my-4 border border-[#171717] py-3 px-4 rounded-md bg-[#0A0A0A]/50 text-[#D4D4D4] backdrop-blur-md">
-                <a href="/" className="flex gap-3 items-center w-full">
-                        <div className="w-8 h-8 relative">
-                                <Image
-                                    src="/doplosymbollogo.svg"
-                                    alt="Doplo Symbol log"
-                                    fill
-                                />
-                        </div>
-                        <h1 className="font-semibold">Doplo</h1>
-                </a>
+                <div className="w-full">
+                    <a href="/" className="flex gap-3 items-center w-fit">
+                            <div className="w-8 h-8 relative">
+                                    <Image
+                                        src="/doplosymbollogo.svg"
+                                        alt="Doplo Symbol log"
+                                        fill
+                                    />
+                            </div>
+                            <h1 className="font-semibold">Doplo</h1>
+                    </a>
+                </div>
                 <div className="hidden md:flex space-x-4">
                     <Link href="/docs">Documentation</Link>
                     <Link href="/showcase">Showcase</Link>
@@ -22,6 +24,9 @@ const Navbar = () => {
                     <Link href="/changelog">Changelog</Link>                    
                 </div>
                 <div className="w-full flex justify-end">
+                    <a target="_blank" rel="noreferrer" href="https://x.com/doplo_ui">
+                        <X size={20}/>
+                    </a>
                     <a target="_blank" rel="noreferrer" href="https://github.com/Ayusht0mar/doplo">
                         <Github size={20}/>
                     </a>
