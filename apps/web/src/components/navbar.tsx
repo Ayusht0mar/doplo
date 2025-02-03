b1bbfe3b4c6a03057b4d80ad468d4a672e4dabcf
Ayush tomar <ayushtomar388@gmail.com>
2025-02-03 23:43:41 +0530

    Style and Add: X logo

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,10 +1,10 @@
-import { Github, X} from "lucide-react";
+import { Github} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
     return ( 
-        <div className="flex sticky top-4 z-10 items-center justify-between w-[94vw] mx-auto max-w-7xl my-4 border border-[#171717] py-3 px-4 rounded-md bg-[#0A0A0A]/50 text-[#D4D4D4] backdrop-blur-md">
+        <div className="flex sticky top-4 z-10 items-center justify-between w-[94vw] mx-auto max-w-7xl my-4 border border-[#171717] py-3 px-4 rounded-md bg-[#0A0A0A]/50 text-neutral-200 backdrop-blur-md">
                 <div className="w-full">
                     <a href="/" className="flex gap-3 items-center w-fit">
                             <div className="w-8 h-8 relative">
@@ -23,11 +23,11 @@ const Navbar = () => {
                     <Link href="/roadmap">Roadmap</Link>
                     <Link href="/changelog">Changelog</Link>                    
                 </div>
-                <div className="w-full flex justify-end">
+                <div className="w-full flex justify-end gap-2">
                     <a target="_blank" rel="noreferrer" href="https://x.com/doplo_ui">
-                        <X size={20}/>
+                        <Image src="/xlogo.svg" alt="X Logo" width={20} height={20}/>
                     </a>
-                    <a target="_blank" rel="noreferrer" href="https://github.com/Ayusht0mar/doplo">
+                    <a target="_blank" rel="noreferrer" href="https://github.com/Ayusht0mar/doplo" >
                         <Github size={20}/>
                     </a>
                 </div>
