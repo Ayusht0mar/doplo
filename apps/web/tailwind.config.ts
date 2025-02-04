13211d6b33c33fa75267b4db0d4239a3c807e05c
Ayush tomar <ayushtomar388@gmail.com>
2025-02-04 23:48:30 +0530

    Update: config change for doplo

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,11 +1,20 @@
-// tailwind config is required for editor support
-
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
-const config: Pick<Config, "content" | "presets"> = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+const config: Pick<Config, "content" | "presets" | "darkMode" | "plugins" | "theme" | "darkMode"> = {
+  darkMode: "selector",
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}" , "./src/components/**/*.{js,ts,jsx,tsx,mdx}",   "../../node_modules/doplo/**/*.{js,ts,jsx,tsx,mdx}" ],
   presets: [sharedConfig],
+  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+
 };
 
 export default config;
