dabf22d8789bd8c6dedc6689a4521a5e06789615
Ayush tomar <ayushtomar388@gmail.com>
2025-02-05 02:52:59 +0530

    Style

--- apps/web/src/app/roadmap/page.tsx
+++ apps/web/src/app/roadmap/page.tsx
@@ -20,7 +20,7 @@ const RoadmapPage = async () => {
   });
 
   return (
-    <div className="flex flex-col w-[94vw] max-w-7xl mx-auto min-h-svh gap-8 bg-[#0C0C0C] rounded-3xl border border-[#171717]">
+    <div className="flex flex-col w-[94vw] max-w-7xl mx-auto min-h-svh pb-5 gap-8 bg-[#0C0C0C] rounded-3xl border border-[#171717]">
       <div className="pt-12 lg:pt-24 px-4 lg:px-12 space-y-4">
         <h1 className="text-4xl font-bold">Roadmap</h1>
         <p className="text-[#C0C0C0] text-balance">
