10612e7315b39cea16204beaeed84208f467b64e
Ayush tomar <ayushtomar388@gmail.com>
2025-02-09 02:14:55 +0530

    Style

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "../components/navbar";
 import { Analytics } from "@vercel/analytics/react"
+import "doplo/index.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,16 +18,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body className={inter.className}>
-        <div className="absolute top-0 z-[-2] min-h-svh w-full bg-[#000000] bg-[radial-gradient(#ffffff20_1px,#000_1px)] bg-[size:20px_20px]"> 
-          <Navbar/>
-          {children}
-          <Analytics/>
-          <p className="text-center text-balance w-full my-6">
-                    Build by <a href="https://www.ayushtomar.in/">Ayush Tomar.</a> The source code is available on <a href="https://github.com/Ayusht0mar/doplo">Github.</a>
-          </p> 
-        </div>
+          <div className="absolute top-0 z-[-2] min-h-svh w-full bg-[#000000] bg-[radial-gradient(#ffffff20_1px,#000_1px)] bg-[size:20px_20px]"> 
+            <Navbar/>
+            {children}
+            <Analytics/>
+            <p className="text-center text-balance w-full my-6">
+                      Build by <a href="https://www.ayushtomar.in/">Ayush Tomar.</a> The source code is available on <a href="https://github.com/Ayusht0mar/doplo">Github.</a>
+            </p> 
+          </div>
       </body>
     </html>
   );
